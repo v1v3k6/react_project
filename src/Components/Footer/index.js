bae9a626df1d85faf8ec2825eeec7811dc6faa9f
v1v3k6 <vivekbiswas@skava.com>
2019-12-18 15:23:39 +0530

    Update: Remove unused lines from the source

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,12 +1,7 @@
 import React from 'react'
-import RenderTopOptionSet from 'src/Components/TopOptions'
-import { IoIosBasket as Logo } from 'react-icons/io';
 import './index.scss'
 
 class Footer extends React.Component{
-    renderPageLogo = () => {
-        return <Logo />
-    }
     generateFooterRibbon = () => {
         return (
             <div className="footer-parent d-flex justify-content-between w-100 mx-auto">
