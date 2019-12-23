bd9ad4b046e1cf79bdf9a666d36bdc35dd4f9efa
raghul11 <54578718+raghul11@users.noreply.github.com>
2019-12-23 10:03:39 +0530

    Update hotel_data.js

--- localDataStore/hotel_data.js
+++ localDataStore/hotel_data.js
@@ -1,21 +1,149 @@
 const dataObject = {
     "hotels": [
         {
-            "name": "",
-            "type": "",
+            "name": "Food Dharbar",
+            "type": "North Indian Style",
             "image": "",
             "dishes": [
                 {
-                    "name": "",
-                    "price": 0,
-                    "image": "",
+                    "name": "Roti",
+                    "price": 25,
+                    "image": "https://cookingwithshazia.com/wp-content/uploads/2019/03/DSC03639.jpg",
                     "description": "",
-                    "type": ""
+                    "type": "Veg"
                 },
+                {
+                    "name": "Masala paapad",
+                    "price": 30,
+                    "image": "https://img1.exportersindia.com/product_images/bc-full/2018/11/2779023/masala-papad-1542370289-4461380.png",
+                    "description": "",
+                    "type": "Veg"
+                },
+                {
+                    "name": "Malai Chicken",
+                    "price": 150,
+                    "image": "https://previews.123rf.com/images/tang90246/tang902461704/tang90246170400233/76240042-chicken-malai-tikka-boneless-piece-indian-dish-.jpg",
+                    "description": "",
+                    "type": "Non-Veg"
+                },
+                {
+                    "name": "Pulav",
+                    "price": 250,
+                    "image": "https://www.sanjeevkapoor.com/UploadFiles/RecipeImages/Masoor-Dal-Berries-Chicken-Pulao-Mummy-Ka-Magic-FoodFood.jpg",
+                    "description": "",
+                    "type": "Non-Veg"
+                }
             ]
         },
+		{
+            "name": "Shree vilas",
+            "type": "South Indian",
+            "image": "",
+            "dishes": [
+                {
+                    "name": "Idly(2-pcs)",
+                    "price": 20,
+                    "image": "",
+                    "description": "",
+                    "type": "Veg"
+                },
+                {
+                    "name": "Dosa",
+                    "price": 50,
+                    "image": "",
+                    "description": "",
+                    "type": "Veg"
+                },
+                {
+                    "name": "Pongal",
+                    "price": 50,
+                    "image": "",
+                    "description": "",
+                    "type": "Veg"
+                },
+                {
+                    "name": "Vada",
+                    "price": 25,
+                    "image": "",
+                    "description": "",
+                    "type": "Veg"
+                }
+            ]
+        },
+		{
+            "name": "Peking",
+            "type": "Chinese",
+            "image": "",
+            "dishes": [
+                {
+                    "name": "Whole Roast Duck",
+                    "price": 201,
+                    "image": "",
+                    "description": "",
+                    "type": "Non-Veg"
+                },
+                {
+                    "name": "Chicken Dynamite",
+                    "price": 125,
+                    "image": "",
+                    "description": "",
+                    "type": "Non-Veg"
+                },
+                {
+                    "name": "Chicken Haka Noodles",
+                    "price": 140,
+                    "image": "",
+                    "description": "",
+                    "type": "Non-Veg"
+                },
+                {
+                    "name": "Manchow soup",
+                    "price": 70,
+                    "image": "",
+                    "description": "",
+                    "type": "Non-Veg"
+                }
+            ]
+        },
+		{
+            "name": "Chit-Chat",
+            "type": "Fast Foods",
+            "image": "",
+            "dishes": [
+                {
+                    "name": "Bhel Poori",
+                    "price": 30,
+                    "image": "http://www.cooktube.in/wp-content/uploads/2016/10/Bhel-Puri.jpg",
+                    "description": "",
+                    "type": "Veg"
+                },
+                {
+                    "name": "Masal Poori",
+                    "price": 30,
+                    "image": "https://3.bp.blogspot.com/-L8Yx4J_id9Y/Wldv3ux046I/AAAAAAAAe9o/6CYde7JavA8eBid7osP5h7_oPT98ZcplQCEwYBhgL/s1600/153.jpg",
+                    "description": "",
+                    "type": "Veg"
+                },
+                {
+                    "name": "Mushroom Fry",
+                    "price": 35,
+                    "image": "https://i.ytimg.com/vi/nOK3ytqRBpU/hqdefault.jpg",
+                    "description": "",
+                    "type": "Veg"
+                },
+                {
+                    "name": "Masala Kachori",
+                    "price": 35,
+                    "image": "https://static.punjabkesari.in/multimedia/2017_2image_11_11_042766775kaju-ll.jpg",
+                    "description": "",
+                    "type": "Veg"
+                }
+            ]
+        },
+		
     ]
 }
+
 
 export default dataObject
 export {dataObject}
